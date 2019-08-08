a28522900b27f7c5825f60ecf3bcd7882ed2264d
leenahere <lina.pete@gmail.com>
2019-08-08 10:07:17 +0200

    tests with max unpooling and keras preprocessing

--- autoencoder/autoencoder.py
+++ autoencoder/autoencoder.py
@@ -1,44 +1,64 @@
-from keras.layers import Permute, Softmax, Conv2D, Flatten, MaxPool2D, UpSampling2D
+from keras.layers import Permute, Softmax, Conv2D, Flatten, MaxPool2D, UpSampling2D, Reshape, Activation
 from keras.models import Sequential, Model
 from keras.optimizers import Adam
+from keras.preprocessing.image import load_img, img_to_array
 import cv2
 import numpy as np
+from max_unpooling_layers import MaxPoolingWithArgmax2D, MaxUnpooling2D
+
+img = load_img('/home/mechlab/hackathon/data/features/um_000000.png', color_mode="grayscale")
+# report details about the image
+print(type(img))
+print(img.format)
+print(img.mode)
+print(img.size)
+
+img_array = img_to_array(img)
+print(img_array.dtype)
+print(img_array.shape)
 
 X_train = np.expand_dims(cv2.imread('/home/mechlab/hackathon/data/features/um_000000.png'), 0)
 y_train = np.expand_dims(cv2.imread('/home/mechlab/hackathon/data/labels_filtered/label_um_000000.png')[:, :, 0], 0)
 
 autoencoder = Sequential()
 # autoencoder.add(Input(shape=(400, 200, 1)))
-autoencoder.add(Conv2D(filters=32, kernel_size=(3, 3), activation='elu', strides=(1, 1), padding='same'))
-# autoencoder.add(Conv2D(filters=32, kernel_size=(3, 3), activation='elu', strides=(1, 1), padding='same'))
-# autoencoder.add(MaxPool2D(pool_size=(2, 2), strides=(2, 2)))
-# autoencoder.add(
-#     Conv2D(filters=128, kernel_size=(3, 3), padding='same', activation='elu', dilation_rate=(1, 1)))
-# autoencoder.add(
-#     Conv2D(filters=128, kernel_size=(3, 3), padding='same', activation='elu', dilation_rate=(1, 2)))
-# autoencoder.add(
-#     Conv2D(filters=128, kernel_size=(3, 3), padding='same', activation='elu', dilation_rate=(1, 2)))
-# autoencoder.add(
-#     Conv2D(filters=128, kernel_size=(3, 3), padding='same', activation='elu', dilation_rate=(1, 2)))
-# autoencoder.add(
-#     Conv2D(filters=128, kernel_size=(3, 3), padding='same', activation='elu', dilation_rate=(1, 2)))
-# autoencoder.add(
-#     Conv2D(filters=128, kernel_size=(3, 3), padding='same', activation='elu', dilation_rate=(1, 2)))
-# autoencoder.add(
-#     Conv2D(filters=128, kernel_size=(3, 3), padding='same', activation='elu', dilation_rate=(1, 2)))
-# autoencoder.add(Conv2D(filters=32, kernel_size=(1, 1)))
+autoencoder.add(Conv2D(filters=32, kernel_size=(3, 3), input_shape=img_array.shape, activation='elu', strides=(1, 1), padding='same'))
+conv_first = Conv2D(filters=32, kernel_size=(3, 3), activation='elu', strides=(1, 1), padding='same')
+autoencoder.add(conv_first)
+max_pool_1, mask_1 = MaxPoolingWithArgmax2D((2, 2))(conv_first)
+autoencoder.add(max_pool_1)
+autoencoder.add(MaxPool2D(pool_size=(2, 2), strides=(2, 2)))
+autoencoder.add(
+    Conv2D(filters=128, kernel_size=(3, 3), padding='same', activation='elu', dilation_rate=(1, 1)))
+autoencoder.add(
+    Conv2D(filters=128, kernel_size=(3, 3), padding='same', activation='elu', dilation_rate=(1, 2)))
+autoencoder.add(
+    Conv2D(filters=128, kernel_size=(3, 3), padding='same', activation='elu', dilation_rate=(1, 2)))
+autoencoder.add(
+    Conv2D(filters=128, kernel_size=(3, 3), padding='same', activation='elu', dilation_rate=(1, 2)))
+autoencoder.add(
+    Conv2D(filters=128, kernel_size=(3, 3), padding='same', activation='elu', dilation_rate=(1, 2)))
+autoencoder.add(
+    Conv2D(filters=128, kernel_size=(3, 3), padding='same', activation='elu', dilation_rate=(1, 2)))
+autoencoder.add(
+    Conv2D(filters=128, kernel_size=(3, 3), padding='same', activation='elu', dilation_rate=(1, 2)))
+conv = Conv2D(filters=32, kernel_size=(1, 1))
+autoencoder.add(conv)
 # autoencoder.add(UpSampling2D(size=(2, 2)))
-# autoencoder.add(Conv2D(filters=32, kernel_size=(3, 3), activation='elu', strides=(1, 1), padding='same'))
-# autoencoder.add(Conv2D(filters=2, kernel_size=(3, 3), activation='elu', strides=(1, 1), padding='same'))
-# autoencoder.add(Permute(dims=(1, 2, 0)))
-# autoencoder.add(Softmax(axis=-1))
+max_unpooling_1 = MaxUnpooling2D((2,2))([conv, mask_1])
+autoencoder.add(max_unpooling_1)
+autoencoder.add(Conv2D(filters=32, kernel_size=(3, 3), activation='elu', strides=(1, 1), padding='same'))
+autoencoder.add(Conv2D(filters=2, kernel_size=(3, 3), activation='elu', strides=(1, 1), padding='same'))
+autoencoder.add(Reshape((2, 400 * 200)))
+autoencoder.add(Permute(dims=(2,1)))
+autoencoder.add(Activation('softmax'))
 
-autoencoder.build(input_shape=(400, 200, 1))
+#autoencoder.build(input_shape=(400, 200, 1))
 print autoencoder.summary()
 
-# autoencoder.compile(loss='categorical_crossentropy', optimizer=Adam(lr=0.01), metrics=['accuracy'])
+autoencoder.compile(loss='categorical_crossentropy', optimizer=Adam(lr=0.01), metrics=['accuracy'])
 # TODO: replace with testing data
-# autoencoder.fit(X_train, y_train, batch_size=4, epochs=5, validation_data=(X_train, y_train))
+#autoencoder.fit(X_train, y_train, batch_size=4, epochs=5, validation_data=(X_train, y_train))
 
 # input_img = Input(shape=(400, 200, 1))
 # encoded1 = Conv2D(filters=32, kernel_size=(3, 3), activation='elu', strides=(1, 1), padding='same')(input_img)
